662871df54b74a248096dbbf1f37e94b90d4cb1d
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-17 06:26:04 -0500

    feat: button secondary

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-const App = () => {
-  const syle = {
-    backgroundColor: '#f8f8f8',
-  };
-  return (
-    <section style={syle}>
+const App = () => (
+  <div>
+    <section>
+      <h1 className="section-heading">Titulo</h1>
       <button type="button" className="btn-primary">Boton principal</button>
     </section>
-  );
-};
+    <section>
+      <button type="button" className="btn-secondary">Boton secundario</button>
+    </section>
+  </div>
+);
 
 export default App;
