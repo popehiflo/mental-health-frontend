59c276896cabdffcad40e4a08a7dbe5f0bd07050
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-17 06:02:14 -0500

    feat: button primary

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-const App = () => (
-  <div>
-    Hello world!
-  </div>
-);
+const App = () => {
+  const syle = {
+    backgroundColor: '#f8f8f8',
+  };
+  return (
+    <section style={syle}>
+      <button type="button" className="btn-primary">Boton principal</button>
+    </section>
+  );
+};
 
 export default App;
