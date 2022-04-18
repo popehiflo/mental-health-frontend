b74341686b98e8af4263c17f2fb8c35812aff1e0
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-18 00:22:28 -0500

    feat: rutas basicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
 const App = () => (
-  <div>
-    <section>
-      <h1 className="section-heading">Titulo</h1>
-      <button type="button" className="btn-primary">Boton principal</button>
-    </section>
-    <section>
-      <button type="button" className="btn-secondary">Boton secundario</button>
-    </section>
-  </div>
+  <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<h1 style={{ marginTop: '100px' }}>Home</h1>} />
+      <Route path="/cart" element={<h1 style={{ marginTop: '100px' }}>Cart</h1>} />
+      <Route path="/services" element={<h1 style={{ marginTop: '100px' }}>All Services</h1>} />
+      <Route path="/services/:category" element={<h1 style={{ marginTop: '100px' }}>Service: category</h1>} />
+      <Route path="/services/:serviceId" element={<h1 style={{ marginTop: '100px' }}>Service: service Id</h1>} />
+      <Route path="/sign-in" element={<h1 style={{ marginTop: '100px' }}>Sign In</h1>} />
+      <Route path="/sign-up" element={<h1 style={{ marginTop: '100px' }}>Sign Up</h1>} />
+
+      <Route path="/admin-panel" element={<h1 style={{ marginTop: '100px' }}>Admin Panel</h1>} />
+
+      <Route path="*" element={<h1 style={{ marginTop: '100px' }}>Page Not Found</h1>} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
