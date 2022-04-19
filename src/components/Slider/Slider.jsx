3e84f553286fea1c9bef60dece7fc39847e3e51c
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-18 19:58:02 -0500

    feat: Slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { useState } from 'react';
 import styledComponents from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { dataSliders } from '../../dataFake';
+import Slide from '../Slide/Slide';
 
 const SliderWrapper = styledComponents.div`
   width: 100%;
@@ -9,7 +11,6 @@ const SliderWrapper = styledComponents.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  background-color: var(--primary-color);
 `;
 
 const Arrow = styledComponents.div`
@@ -25,28 +26,42 @@ const Arrow = styledComponents.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${(props) => (props.direction === 'left' && '10px')};
-  right: ${(props) => (props.direction === 'right' && '10px')};
+  left: ${(props) => (props.direction === 'left' && '1rem')};
+  right: ${(props) => (props.direction === 'right' && '1rem')};
   margin: auto;
   cursor: pointer;
   opacity: 0.9;
   z-index: 2;
 `;
 
+const SliderContainer = styledComponents.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(-${(props) => props.slideIndex * 100}vw);
+`;
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
   const handleArrowClick = (direction) => {
     if (direction === 'left') {
-      console.log('left');
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
     }
   };
 
   return (
     <SliderWrapper>
-      <Arrow direction="left" onClick={handleArrowClick('left')}>
+      <Arrow direction="left" onClick={() => handleArrowClick('left')}>
         <FontAwesomeIcon icon={faChevronLeft} />
       </Arrow>
-      <div>Slider</div>
-      <Arrow direction="right" onClick={handleArrowClick('right')}>
+      <SliderContainer slideIndex={slideIndex}>
+        {dataSliders.map((slideData) => (
+          <Slide key={slideData.idSlide} contentSlide={slideData} />
+        ))}
+      </SliderContainer>
+      <Arrow direction="right" onClick={() => handleArrowClick('right')}>
         <FontAwesomeIcon icon={faChevronRight} />
       </Arrow>
     </SliderWrapper>
