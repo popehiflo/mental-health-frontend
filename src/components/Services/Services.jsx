5e29a72f68d405f0a967672ed1f14f7d2d06a56c
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-19 12:44:41 -0500

    feat: Services Component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import styledComponents from 'styled-components';
+import { dataPopularServices } from '../../dataFake';
+import ServiceCard from '../ServiceCard/ServiceCard';
 
 const ServicesContainer = styledComponents.section``;
 
+const ContainerBox = styledComponents.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  gap: 2rem;
+`;
+
 const Services = () => (
-  <ServicesContainer>Services</ServicesContainer>
+  <ServicesContainer>
+    <h1 className="section-heading">
+      servicios
+      {' '}
+      <span>
+        más buscados
+      </span>
+    </h1>
+    <ContainerBox>
+      {dataPopularServices.map((service) => (
+        <ServiceCard key={service.id} service={service} />
+      ))}
+    </ContainerBox>
+  </ServicesContainer>
 );
 
 export default Services;
