f50d4fcbab13564aa52a14cea21be3d66fafbc41
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-19 01:20:01 -0500

    feat: created Services, ServiceCard Components

---
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styledComponents from 'styled-components';
+
+const ServiceBox = styledComponents.div``;
+
+const ServiceCard = () => (
+  <ServiceBox>ServiceCard</ServiceBox>
+);
+
+export default ServiceCard;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styledComponents from 'styled-components';
+
+const ServicesContainer = styledComponents.section``;
+
+const Services = () => (
+  <ServicesContainer>Services</ServicesContainer>
+);
+
+export default Services;
